a2dfc1181c378dda5fe1b197ed115f717ccb4502
Pietrek101 <piterbanasik@wp.pl>
2023-10-25 11:51:39 +0200

    25.10.2023

--- lekcja 1-struktura programu/Struktura_programow.cpp
+++ lekcja 1-struktura programu/Struktura_programow.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 int main(){
-cout <<"Hello World!"<<endl;
-cout <<"Imie";
+	short liczba = 2;	//liczba podana przeze mnie
+	float liczbauzytkownika;  	//liczba podawana przez uzytkownika
+	cout <<"Hello World!" << endl; 
+	cout <<"Imie " << liczba; //wyswietlenie zmiennej
+	
+	cout << endl;
+	cout <<"Podaj liczbe: "; //Pytanie o liczbe
+	cin >> liczbauzytkownika; //podawanie liczby
+	
+	cout << endl;
+	cout << "Podana liczba: " << liczbauzytkownika; 
+	/* 
+	komentarz na wiele linijek
+	wiele linijek
+	*/
     return 0;
 }
