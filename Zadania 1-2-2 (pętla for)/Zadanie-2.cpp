ca3afee1697134251ff322e5ab8b5fce47aab389
Pietrek101 <piterbanasik@wp.pl>
2023-11-13 15:02:05 +0100

    13.11.2023

--- Zadania 1-2-2 (pętla for)/Zadanie-2.cpp
+++ Zadania 1-2-2 (pętla for)/Zadanie-2.cpp
@@ -3,15 +3,16 @@ using namespace std;
 int main(){
 	int a = 0;
 	int b = 0;
-	cout << "Podaj dlugosc: ";
-	cin >> a;
 	cout << "Podaj szerokosc: ";
+	cin >> a;
+	cout << "Podaj wysokosc: ";
 	cin >> b;
-	for(int i=a; i < a; i++){
-		for(int j=b; j < b; i++){
-			cout << "*" << endl;
+	for(int i=0; i < a; i++){
+		for(int j=0; j < b; j++){
+			if(j = 0,b) cout << "*";
+			else cout << " ";
 		}
-	cout << "*" << endl;
+	cout << endl;
 	}
 	return 0;
 }
