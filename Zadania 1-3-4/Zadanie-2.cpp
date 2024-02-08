9ae4a006ac946a93d3c1284c77a70291f6d6d10f
Pietrek101 <148774117+Pietrek101@users.noreply.github.com>
2024-02-08 09:01:05 +0100

    Add files via upload

--- Zadania 1-3-4/Zadanie-2.cpp
+++ Zadania 1-3-4/Zadanie-2.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 int suma(int a,int b){
 	return a+b;}
-double suma(int a, float b){
+float suma(int a, float b){
 	return a+b;}
-double suma(float a,int b){
+float suma(float a,int b){
 	return a+b;}
-double suma(float a,float b){
+float suma(float a,float b){
 	return a+b;}
 int main(){
+	float a,b;
 	cout << "Podaj pierwsza liczbe: " << endl;
-	cin 
+	cin >> a;
 	cout << "Podaj druga liczbe: " << endl;
-	
+	cin >> b;
+	cout << "Suma liczb: " << suma(a,b);
 	return 0;}
