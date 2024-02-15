44921702b0e5eacf0b143f41e4291b03492832a5
Pietrek101 <148774117+Pietrek101@users.noreply.github.com>
2024-02-15 09:13:29 +0100

    Add files via upload

--- Zadanie 1-3-5/Zadanie-3.cpp
+++ Zadanie 1-3-5/Zadanie-3.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 using namespace std;
-void abcd (int *d){
-    *d+= 1;
-    *d+= 2;
-    *d+= 3;
+int e = 1;
+void liczba (float *d){
+    *d += e;
+    e += 1;
 }
 int main(){
 	float a = 1;
 	float b = 1;
 	float c = 1;
-	int *wsk1 = &a;
-	int *wsk2 = &b;
-	int *wsk3 = &c;
+	float *wsk1 = &a;
+	float *wsk2 = &b;
+	float *wsk3 = &c;
 	cout << "wartosci zmiennych: " << endl;
-	cout << "a: " << a << "adres obiektu: " <<  endl;
-	cout << "b: " << b << endl;
-	cout << "c: " << c << endl;
+	cout << "a: " << a << " adres obiektu: " << &a <<  endl;
+	cout << "b: " << b << " adres obiektu: " << &b << endl;
+	cout << "c: " << c << " adres obiektu: " << &c << endl;
 	cout << "wartosci zmiennych po modyfikacji: " << endl;
-	cout << "a: " << *d << endl;
-	cout << "b: " << *d << endl;
-	cout << "c: " << *d << endl;
+	liczba(wsk1);
+	cout << "a: " << a << endl;
+	liczba(wsk2);
+	cout << "b: " << b << endl;
+	liczba(wsk3);
+	cout << "c: " << c << endl;
 }
