e7c21003f7a818a792ba5d22c0578282254b4b29
Pietrek101 <148774117+Pietrek101@users.noreply.github.com>
2024-04-15 12:51:50 +0200

    Add files via upload

---
+++ Zadanie 2-7-1/Zadanie 2-7-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	int k = 2;
+	cout << "Podaj liczbe: " << endl;
+	cin >> n;
+	cout << "Czynniki pierwsze liczby: " << n << ": " << endl;
+	for(n > 1;;){
+		for(n / k == 0;;){
+			if(n % k == 0){
+				cout << k << " ";
+				n = n / k;}
+			else{
+				k++;}
+		}
+	}
+	return 0;
+}
